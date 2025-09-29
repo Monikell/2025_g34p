7ed74e41d2f715c9f4dd2ba567535c7aace5b0b3
Monikell <monikell@ttu.edu>
2025-09-29 15:38:21 -0500

    data upload

--- scripts/data_check.R
+++ scripts/data_check.R
@@ -1,12 +1,11 @@
 
 
 # libraries
-install.packages("photosynthesis")
 library(photosynthesis)
-
 library(readr)
 library(dplyr)
-
+library(gasanalyzer)
+install.packages("gasanalyzer")
 
 # read in data -----------------------------------------------------------------
 
@@ -14,7 +13,8 @@ library(dplyr)
 ## using "read_licor", doesn't work on a few of the machines -----------------------------------------------------------------------
 
 ## read individual files
-albert_9.20 <- read_licor("../data/li_6800/g34p_albert/2025-09-20-0934_logdata_albert")
+albert_9.20 <- read_6800_txt("../data/li_6800/g34p_albert/2025-09-20-0934_logdata_albert")
+gibson_9.20 <- read.delim("../data/li_6800/g34p_gibson/2025-09-20-1105_logdata", skip = 63, header = TRUE)
 
 
 
@@ -35,7 +35,8 @@ files_albert <- files_albert[!grepl("\\.xlsx?$", files_albert,
                                     ignore.case = TRUE)]
 
 ## 3 applying the read lifore to the files
-data_albert <- lapply(files_albert, read_licor)
+data_albert <- lapply(files_albert, read_6800_txt)
+
 
 ## 4 making one big albert files to check ID's
 data_albert_df <- bind_rows(data_albert, .id = "file_id")
@@ -49,11 +50,17 @@ files_gibson <- list.files(gibson, full.names = TRUE)
 ## 2
 files_gibson <- files_gibson[!grepl("\\.xlsx?$", files_gibson, 
                                     ignore.case = TRUE)]
+
 ## 3
-data_gibson <- lapply(files_gibson, read_licor)
+data_gibson <- lapply(files_gibson, read_6800_txt)
+
 
 ## 4
 data_gibson_df <- bind_rows(data_gibson, .id = "file_id")
+
+
+files_gibson
+file.info(files_gibson)$isdir
 
 
 
@@ -65,7 +72,7 @@ files_ozzie <- list.files(ozzie, full.names = TRUE)
 files_ozzie <- files_ozzie[!grepl("\\.xlsx?$", files_ozzie, 
                                     ignore.case = TRUE)]
 ## 3
-data_ozzie <- lapply(files_ozzie, read_licor)
+data_ozzie <- lapply(files_ozzie, read_6800_txt)
 
 ## 4
 data_ozzie_df <- bind_rows(data_ozzie, .id = "file_id")
@@ -80,7 +87,7 @@ files_yadi <- list.files(yadi, full.names = TRUE)
 files_yadi <- files_yadi[!grepl("\\.xlsx?$", files_yadi, 
                                   ignore.case = TRUE)]
 ## 3
-data_yadi <- lapply(files_yadi, read_licor)
+data_yadi <- lapply(files_yadi, read_6800_txt)
 
 ## 4
 data_yadi_df <- bind_rows(data_yadi, .id = "file_id")
@@ -96,50 +103,8 @@ files_stan <- list.files(stan, full.names = TRUE)
 files_stan <- files_stan[!grepl("\\.xlsx?$", files_stan, 
                                 ignore.case = TRUE)]
 ## 3
-data_stan <- lapply(files_stan, read_licor)
+data_stan <- lapply(files_stan, read_6800_txt)
 
 ## 4
 data_stan_df <- bind_rows(data_stan, .id = "file_id")
 
-
-unique(data_albert_df$id)
-unique(data_ozzie_df$id)
-
-
-## using GasanalyzeR
-
-install.packages("gasanalyzer")
-library(gasanalyzer)
-
-exampleFiles <- system.file("extdata", package = "gasanalyzer")
-import_factory_cals(exampleFiles)
-
-xlsxfile <- read_6800_xlsx(paste(exampleFiles, "lowo2.xlsx", sep = "/"))
-txtfile <- read_6800_txt(paste(exampleFiles, "lowo2", sep = "/"))
-
-
-all.equal(txtfile[!excludeCols], xlsxfile[!excludeCols], tolerance = 1e-6)
-
-
-# default points:
-update_geom_defaults("point", list(shape = 1, size = 3))
-
-# convenience function to change plot labels automatically:
-nice_labels <- function(plt) {
-  plt$labels <- var2label(plt$labels)
-  plt
-}
-
-#generate some descriptive stats  and plotmath it with a call to var2label:
-descr <- colMeans(xlsxfile[c("LeafQ.Qin", "Meas.Tleaf", "Const.Oxygen")])
-dlist <- var2label(rep(names(descr), 2),
-                   use_units = rep(c(FALSE, TRUE), each = 3),
-                   val = c(rep(NA, 3), as.character(descr)),
-                   group = c("", "")) |>
-  setNames(letters[1:6]) |> unlist() |>
-  substitute(a==d*","~b==e*","~c==f, env = _ ) |> as.expression()
-
-AvsCi1 <- xlsxfile |> ggplot(aes(GasEx.Ci, GasEx.A)) + geom_point() +
-  labs(title = dlist)
-
-nice_labels(AvsCi1)
