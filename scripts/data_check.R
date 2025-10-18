48893f5f0e6df74e417bf5d29b9dc3f804097d0c
Monikell <monikell@ttu.edu>
2025-10-18 16:47:40 -0500

    looking for any MIA individuals

--- scripts/data_check.R
+++ scripts/data_check.R
@@ -9,6 +9,7 @@
 
 ## LEMONPIE (run ANOVA to see how much of a difference there is)
 
+
 # libraries --------------------------------------------------------------------
 library(photosynthesis)
 library(readr)
@@ -21,13 +22,14 @@ library(readxl)
 library(purrr)
 library(tidyr)
 library(stringr)
+library(lubridate)
 
 # data -------------------------------------------------------------------------
 
 ## individual plant ids
 id_list_plants <- read.csv("../data_meta/plant_ids.csv")
 
-# data: gas exchange -----------------------------------------------------------
+# data: gas exchange ----------------------------------------------------------- 
 
 ## Creating file paths for each machine
 albert <- "../data/li_6800/g34p_albert/"
@@ -189,9 +191,10 @@ ids_gibson$machine <- "gibson"
 
 id_list_licor <- rbind(ids_yadi, ids_stan, ids_ozzie, ids_albert, ids_gibson)
 
-write.csv(id_list_licor, file = "../data/id_list_licor.csv")
+## mrk notes made
+# write.csv(id_list_licor, file = "../data/id_list_licor.csv")
 
-
+head(id_list_licor)
 
 ## MIA: gas exchange -----------------------------------------------------------
 
@@ -203,9 +206,9 @@ id_list_licor <- id_list_licor %>%
 missing_licor_data <- anti_join(id_list_plants, id_list_licor, 
                                 by = "individual")
 
-missing_licor_data
 
-write.csv(missing_licor_data, "../data/missing_licor_data.csv")
+## mrk notes made
+# write.csv(missing_licor_data, "../data/missing_licor_data.csv")
 
 
 ## running anova --- ########################################################### LEMONPIE
@@ -221,10 +224,6 @@ ozzie_dark <- "../data/li_6800/g34p_ozzie_dark/"
 stan_dark <- "../data/li_6800/g34p_ozzie_dark/"
 yadi_dark <- "../data/li_6800/g34p_yadi_dark/"
 
-# SLA file names
-files_sla <- list.files("../data/sla/", full.names = TRUE)
-files_sla <- as.data.frame(files_sla)
-write.csv(files_sla, "../data/files_sla.csv")
 
 # Albert files
 ## 1 list out all the files
@@ -305,6 +304,8 @@ data_stan_dark <- lapply(files_stan_dark, read_6800_txt)
 ## 4
 data_stan_dark_df <- bind_rows(data_stan_dark, .id = "file_id")
 
+
+head(data_albert_dark_df)
 
 
 ## extracting ID list ---
@@ -391,7 +392,9 @@ id_list_licor_dark <- rbind(ids_yadi_dark,
                             ids_albert_dark, 
                             ids_gibson_dark)
 
-write.csv(id_list_licor_dark, file = "../data/id_list_licor_dark.csv")
+
+## mrk notes made
+# write.csv(id_list_licor_dark, file = "../data/id_list_licor_dark.csv")
 
 
 ## MIA: dark-resp --------------------------------------------------------------
@@ -406,13 +409,126 @@ missing_licor_data_dark <- anti_join(id_list_plants, id_list_licor_dark,
 
 missing_licor_data_dark
 
-write.csv(missing_licor_data_dark, "../data/missing_licor_data_dark.csv")
+
+## mrk notes made
+# write.csv(missing_licor_data_dark, "../data/missing_licor_data_dark.csv")
+
+## other MIA methods -----------------------------------------------------------
+
+### dates ----------------------------------------------------------------------
+## lump by dates
+id_list_licor_summary <- id_list_licor %>%
+  mutate(date = str_extract(SysObs.Filename, "\\d{4}-\\d{2}-\\d{2}")) %>%
+  group_by(date) %>%
+  summarise(individuals = list(unique(individual))) %>%
+  ungroup()
+
+
+ids_day_li6800 <- id_list_licor_summary %>% tidyr::unnest(individuals)
+
+
+
+id_list_licor_dark_summary <- id_list_licor_dark %>%
+  mutate(date = str_extract(SysObs.Filename, "\\d{4}-\\d{2}-\\d{2}")) %>%
+  group_by(date) %>%
+  summarise(individuals = list(unique(individual))) %>%
+  ungroup()
+
+
+ids_night_li6800 <- id_list_licor_dark_summary %>% tidyr::unnest(individuals)
+
+
+head(ids_night_li6800)
+head(ids_day_li6800)
+
+
+## trying to see about individual that don't overlap dates or something
+mia_dark_date <- anti_join(ids_night_li6800, ids_day_li6800, 
+                                     by = "individuals", "date")
+
+
+# write.csv(ids_night_li6800, "../data/mia_data/dark_mia_investigation/li6800_dark_ids.csv")
+# write.csv(ids_day_li6800, "../data/mia_data/dark_mia_investigation/li6800_light_ids.csv")
+
+
+### times ----------------------------------------------------------------------
+
+## stan
+curve_times_stan_dark <- data_stan_dark_df %>%
+  group_by(UserDefCon.id, SysObs.Filename) %>%
+  summarise(
+    start_time = min(SysObs.Time),
+    end_time   = max(SysObs.Time),
+    duration_s = as.numeric(difftime(end_time, start_time, units = "secs")),
+    .groups = "drop"
+  )
+
+
+## albert
+curve_times_albert_dark <- data_albert_dark_df %>%
+  group_by(UserDefCon.id, SysObs.Filename) %>%
+  summarise(
+    start_time = min(SysObs.Time),
+    end_time   = max(SysObs.Time),
+    duration_s = as.numeric(difftime(end_time, start_time, units = "secs")),
+    .groups = "drop"
+  )
+
+
+## yadi
+curve_times_yadi_dark <- data_yadi_dark_df %>%
+  group_by(UserDefCon.id, SysObs.Filename) %>%
+  summarise(
+    start_time = min(SysObs.Time),
+    end_time   = max(SysObs.Time),
+    duration_s = as.numeric(difftime(end_time, start_time, units = "secs")),
+    .groups = "drop"
+  )
+
+## ozzie
+curve_times_ozzie_dark <- data_ozzie_dark_df %>%
+  group_by(UserDefCon.id, SysObs.Filename) %>%  
+  summarise(
+    start_time = min(SysObs.Time),
+    end_time   = max(SysObs.Time),
+    duration_s = as.numeric(difftime(end_time, start_time, units = "secs")),
+    .groups = "drop"
+  )
+
+## gibson
+curve_times_gibson_dark <- data_gibson_dark_df %>%
+  group_by(UserDefCon.id, SysObs.Filename) %>%
+  summarise(
+    start_time = min(SysObs.Time),
+    end_time   = max(SysObs.Time),
+    duration_s = as.numeric(difftime(end_time, start_time, units = "secs")),
+    .groups = "drop"
+  )
+
+
+
+
+## adding machine names for clarity
+curve_times_yadi_dark$machine <- "yadi"
+curve_times_stan_dark$machine <- "stan"
+curve_times_ozzie_dark$machine <- "ozzie"
+curve_times_albert_dark$machine <- "albert"
+curve_times_gibson_dark$machine <- "gibson"
+
+
+curve_times_dark <- rbind(curve_times_yadi_dark, 
+                          curve_times_stan_dark, 
+                          curve_times_ozzie_dark, 
+                          curve_times_albert_dark, 
+                          curve_times_gibson_dark)
+
+write.csv(curve_times_dark, "../data/mia_data/dark_mia_investigation/curve_times_dark.csv")
 
 
 # data: light-flo --------------------------------------------------------------
 
 ## Creating file paths for light fluoro
-files_li600 <- "../data/li_600/export_2025_10_16_160915/g34p_light_v2/"
+files_li600 <- "../data/li_600/export_2025_10_18_142238/g34p_light_v2/"
 
 
 # 1: list all Excel files in that directory *and all sub-folders*
@@ -432,20 +548,14 @@ data_li600_df <- map_dfr(
 )
 
 
-# 3: csv faster sort/ print as needed
-# write.csv(data_li600_df, file = "../data/data_li600_df.csv")
-
-
-# 4: pull only the unique barcodes and id's
+# 3: pull only the unique barcodes and id's
 li600_ids <- data_li600_df %>% 
   distinct(USERDEF...7, USERDEF...8,SYS...2, SYS...3)
 
 
 
-head(li600_ids)
-head(id_list_plants)
  
-## MIA: li600 ------------------------------------------------------------------
+## MIA: light-flo --------------------------------------------------------------
 
 ## updating column names to match
 li600_ids <- li600_ids %>%
@@ -465,9 +575,97 @@ unique(li600_ids$individual)
 missing_li600_data <- anti_join(id_list_plants, li600_ids, 
                                 by = "individual")
 
-missing_li600_data
+## mrk notes made
+# write.csv(missing_li600_data, "../data/missing_li600_data.csv")
+write.csv(data_li600_df, "../data/li600_data.csv")
 
-write.csv(missing_licor_data, "../data/missing_licor_data.csv")
 
 
+# data: dark-flo ---------------------------------------------------------------
+
+## Creating file paths for light fluoro
+files_li600_dark <- "../data/li_600/export_2025_10_18_142238/g34p_dark/"
+
+
+# 1: list all Excel files in that directory *and all sub-folders*
+files_li600_all_dark <- list.files(
+  path = files_li600_dark,
+  pattern = "\\.xlsx?$",
+  full.names = TRUE,          
+  recursive = TRUE, # dive into the sub-folders
+  ignore.case = TRUE
+)
+
+# 2: read & combine
+data_li600_dark_df <- map_dfr(
+  files_li600_all_dark,
+  ~ read_excel(.x),
+  .id = "file_id"
+)
+
+## barcode/ id issue (didn't sep. until later)
+# write.csv(data_li600_dark_df, "../data/mia_data/data_li600_dark.csv")
+
+li600_dark_ids_updated <- read.csv("../data/mia_data/data_li600_dark_v2.csv")
+
+li600_dark_ids_updated$barcode
+
+# 3: pull only the unique barcodes and id's
+li600_dark_ids <- li600_dark_ids_updated %>% 
+  distinct(individual, barcode)
+
+## MIA: dark-flo ---------------------------------------------------------------
+
+
+## compare data
+mia_li600_dark_data <- anti_join(id_list_plants, li600_dark_ids, 
+                                by = "individual")
+
+
+## reupload try agian
+
+
+
+## mrk notes made
+write.csv(mia_li600_dark_data, "../data/mia_data/mia_li600_dark_data.csv")
+# write.csv(data_li600_df, "../data/li600_data.csv")
+
+
+
+
+
+
+
+
+## MIA: SLA --------------------------------------------------------------------
+
+## 1: grab the file name
+files_sla <- list.files("../data/sla/", pattern = "\\.jpg$", full.names = TRUE)
+
+
+## 2: remove the file paths
+files_sla_ids <- basename(files_sla)
+  
+## 3: removes the .jpg by only (* = ignores anything after that)
+files_sla_ids <- sub("^([0-9]+).*", "\\1", files_sla_ids)
+
+## 4: covert them to numeric
+files_sla_ids <- as.numeric(files_sla_ids)
+
+## 5: convert to a dataframe
+files_sla_ids <- as.data.frame(files_sla_ids)
+
+files_sla_ids
+
+## 5: updating column names to match 
+files_sla_ids <- files_sla_ids %>%
+  rename(individual = files_sla_ids)
+
+
+## who is missing
+mia_sla_data <- anti_join(id_list_plants, files_sla_ids, 
+          by = "individual")
+
+## mrk notes made
+# write.csv(mia_sla_data, "../data/missing_data_check/mia_sla_data.csv")
 
