e38cd052061a8ae3a296dff94b0ad1c96b24e2db
Monikell <monikell@ttu.edu>
2026-06-25 11:34:41 -0500

    pheno figures

--- scripts/phenology.R
+++ scripts/phenology.R
@@ -39,9 +39,6 @@ data_pheno_full <- data_pheno_full %>%
     id = as.numeric(id)
   )
 
-## adding plant and block information
-data_pheno_full <- left_join(data_pheno_full, meta_plant,
-                             by = c('id' = 'individual'))
 
 ## removing NA value form id, removes 1 row, which was just a note. 
 data_pheno_full <- data_pheno_full %>%
@@ -52,87 +49,90 @@ data_pheno_full <- data_pheno_full %>%
   rename(height = X9_height_cm,
          date = X1_date)
 
-
 ## removing extra columns
+data_pheno_full <- data_pheno_full %>% 
+  select(id, date, height)
+
 data_pheno_last <- data_pheno_last %>%
-  select(date, id, height, notes)
+  select(date, id, height)
   
-
-## merging data sets entered in Excel
-data_pheno_mid_last <- bind_rows(data_pheno_mid, data_pheno_last)
-
-
-## combining the notes column
-data_pheno_mid_last <- data_pheno_mid_last %>%
-  unite(col = "notes", note, notes, 
-        remove = TRUE,
-        na.rm = TRUE)
-
-# ## merge excel and epicollect data into one!
-# data_pheno_full <- bind_rows(data_pheno_full, data_pheno_mid_last)
+data_pheno_mid <- data_pheno_mid %>%
+  select(date, id, height)
 
 ## convert dates into dates
 data_pheno_full <- data_pheno_full %>%
   mutate(date = as.Date(date, 
-                           format = "%d/%m/%Y"))
+                        format = "%d/%m/%Y"))
 
-## convert dates into dates
-data_pheno_mid_last <- data_pheno_mid_last %>%
+data_pheno_last <- data_pheno_last %>%
   mutate(date = as.Date(date, 
-                           format = "%d/%m/%Y"))
+                        format = "%d/%m/%Y"))
 
-## mergining epicollect and excel data together
-data_pheno_full <- cbind(data_pheno_full, data_pheno_mid_last)
+data_pheno_mid <- data_pheno_mid %>%
+  mutate(date = as.Date(date, 
+                        format = "%d/%m/%Y"))
+
+
+## merging data sets
+data_pheno_heights <- bind_rows(data_pheno_full, data_pheno_last, 
+                                data_pheno_mid)
+
+
+## add in metadata
+data_pheno_heights <- left_join(data_pheno_heights, meta_plant,
+                             by = c('id' = 'individual'))
 
 
 ## adding week #
-data_pheno_full$week_number <- strftime(data_pheno_full$X1_date, 
+data_pheno_heights$week_number <- strftime(data_pheno_heights$date, 
                                         format ='%V')
 
 
-# ## adding weeks
-# data_pheno_full_weeks <- data_pheno_full %>%
-#   mutate(week = case_when(
-#     X1_date %in% c("14/08/2025", "16/08/2025") ~ "week 1",
-#     X1_date %in% c("21/08/2025") ~ "week 2",
-#     X1_date %in% c("28/08/2025") ~ "week 3",
-#     TRUE ~ "other"
-#   ))
+## renaming some of the silly column names I used 
+data_pheno_heights <- data_pheno_heights %>%
+  rename(trt_p = phosphorous_p1.p4,
+         trt_light = shade_00s_70s,
+         photopath = c3_c4)
+
+## prepping data for plotting
+data_pheno_heights$week_number <- as.factor(data_pheno_heights$week_number)
+data_pheno_heights$phosphorous_p1.p4 <- as.factor(data_pheno_heights$trt_p)
+data_pheno_heights$shade_00s_70s <- as.factor(data_pheno_heights$trt_light)
 
 
-# prepping data for plotting
-data_pheno_full$week_number <- as.factor(data_pheno_full$week_number)
-data_pheno_full$phosphorous_p1.p4 <- as.factor(data_pheno_full$phosphorous_p1.p4)
-data_pheno_full$shade_00s_70s <- as.factor(data_pheno_full$shade_00s_70s)
-
-# plotting ---------------------------------------------------------------------
-
-## filtering data we want to focus on
-data_pheno_full_weeks_filtered <- data_pheno_full_weeks %>%
-  filter(week %in% c("week 1", "week 2", "week 3"),
-         phosphorous_p1.p4 %in% c("p1", "p4"))
 
 
-## plotting general leaf counts
-ggplot(data_pheno_full_weeks_filtered, 
-       aes(x = as.factor(week), 
-           y = X7_leaf_count, 
-           fill = as.factor(phosphorous_p1.p4))) +
-  geom_boxplot() +
-  facet_wrap( ~ shade_00s_70s)
+# figures ----------------------------------------------------------------------
+
+## figures data prep -----------------------------------------------------------
+## prepping data, trying to isolate just weeks with biggest observations
+filtered_data <- data_pheno_heights %>%
+  group_by(week_number) %>%
+  filter(n_distinct(id) > 190)
+
+## checking to see unique numbers and names
+test_v2 <- filtered_data %>% 
+  group_by(week_number) %>%
+  summarise(unique_ids = n_distinct(id))
+
+## removing the weird weeks 
+filtered_data <- filtered_data %>% 
+  filter(date != "2025-08-25")
+
+## removing not p1 or p4
+filtered_data_p1_p4 <- filtered_data %>% 
+  filter(trt_p %in% c("p1", "p4"))
 
 
-## plotting c3 vs. c4 general leaf counts
-ggplot(data_pheno_full_weeks_filtered, 
-       aes(x = as.factor(week), 
-           y = X7_leaf_count, 
-           fill = as.factor(phosphorous_p1.p4))) +
-  geom_boxplot() +
-  facet_wrap( ~ c3_c4)
+filtered_data <- data %>%
+  filter(column_name %in% c("value1", "value2"))
 
+### height ---------------------------------------------------------------------
+
+data_pheno_heights
 
 ## c3 vs. c4 plant height
-ggplot(data_pheno_full_weeks_filtered, 
+ggplot(data_pheno_heights_weeks_filtered, 
        aes(x = as.factor(week), 
            y = X8_height_cm, 
            fill = as.factor(phosphorous_p1.p4))) +
@@ -142,7 +142,7 @@ ggplot(data_pheno_full_weeks_filtered,
 
 
 ## species plant height
-ggplot(data_pheno_full_weeks_filtered, 
+ggplot(data_pheno_heights_weeks_filtered, 
        aes(x = as.factor(week), 
            y = X7_leaf_count, 
            fill = as.factor(phosphorous_p1.p4))) +
@@ -150,12 +150,119 @@ ggplot(data_pheno_full_weeks_filtered,
   facet_wrap( ~ c3_c4 + usda_code)
 
 
+## height x weeks: species (v1, straight line)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot () +
+  facet_wrap(~species_code)
+
+## height x weeks: species 
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(group = 1), method = "lm", color = "black", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3)
 
 
-## height how does that look through the weeks (v1)
-ggplot(data_pheno_full, aes(x = week_number, 
-                            y = X9_height_cm,
-                            fill = phosphorous_p1.p4)) +
+## height x weeks: species (v2, curved lines)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(group = 1), method = "loess", color = "black", se = TRUE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+## height x weeks: species (v3, different shades)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(color = trt_light, group = trt_light),
+              method = "lm", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+## height x weeks: species (v4, different shades, cute curves)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(color = trt_light, group = trt_light),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+
+## height x weeks: species (v4, different shades, cute curves) ----------------# fig.3
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(color = trt_light, group = trt_light),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+
+
+## height x weeks: species (v4, different shades, cute curves) ----------------# fig.4
+ggplot(filtered_data_p1_p4, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(color = trt_light, group = trt_light),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+
+
+## height x weeks: species (v5)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(color = trt_light, group = trt_light),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code + trt_p) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+## height x weeks: shade treatments (v1)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
   geom_boxplot() +
-  facet_wrap(~ shade_00s_70s)
+  facet_wrap(~ trt_light) + 
+  scale_fill_grey(start = 0.9, end = 0.3)
 
+
+## height x weeks: shade treatments (v2)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+  geom_smooth(aes(color = trt_light, group = trt_light),
+              method = "loess", se = FALSE) +  
+  facet_wrap(~ trt_light + trt_p) + 
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+## height x weeks: shade treatments (v4) -------------------------------------## fig.1) shade x trt_p (w/ trends)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_smooth(aes(color = trt_p, group = trt_p),
+              method = "loess", se = FALSE) +
+  facet_grid(~ trt_light) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+## height x weeks: shade treatments (v4) -------------------------------------## fig.2) shade x trt p x species (w/ trends)
+ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+  geom_boxplot(alpha = 0.8) +
+  geom_smooth(aes(color = trt_p, group = trt_p),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3)
+
+
+
+
+# write.csv(data_pheno_heights, "../data/r_data/pheight.csv")
