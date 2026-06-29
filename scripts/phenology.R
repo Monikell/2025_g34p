04c0e3768cdb6137f8a8c364d48b09cbe0a0ebe4
Monika Kelley <111803638+Monikell@users.noreply.github.com>
2026-06-29 13:41:39 -0500

    biomass code

--- scripts/phenology.R
+++ scripts/phenology.R
@@ -20,8 +20,11 @@ data_pheno <- read.csv("../data/phenology/branch-1__phenology.csv")
 data_pheno_mid <- read_excel("../data/phenology/plant_height_mid.xlsx")
 data_pheno_last <- read_excel("../data/phenology/plant_height_last_date.xlsx")
 
-## li600 data 
-data_li600_v1 <- read.csv("../data/li_600/g34p_light_v2/2025-10-05/g34p_light_v2_mrk_2025_10_07_15_21_46_1.csv")
+## biomass data 
+shoots <- read_excel("../data/biomass/biomass_shoots.xlsx")
+roots <- read_excel("../data/biomass/biomass_roots_v2.xlsx")
+meta_data <- read.csv("../data_meta/plant_ids.csv")
+biomass_total <-read.csv("../data/r_data/biomass_total.csv")
 
 ## plant and block information
 meta_plant <- read.csv("../data_meta/plant_ids.csv")
@@ -127,97 +130,95 @@ filtered_data_p1_p4 <- filtered_data %>%
   filter(trt_p %in% c("p1", "p4"))
 
 
-filtered_data <- data %>%
-  filter(column_name %in% c("value1", "value2"))
-
 ### height ---------------------------------------------------------------------
 
 data_pheno_heights
+# 
+# ## c3 vs. c4 plant height
+# ggplot(filtered_data, 
+#        aes(x = as.factor(week), 
+#            y = X8_height_cm, 
+#            fill = as.factor(phosphorous_p1.p4))) +
+#   geom_boxplot() +
+#   facet_wrap( ~ c3_c4)
+# 
+# 
+# ## species plant height
+# ggplot(filtered_data, 
+#        aes(x = as.factor(week), 
+#            y = X7_leaf_count, 
+#            fill = as.factor(phosphorous_p1.p4))) +
+#   geom_boxplot() +
+#   facet_wrap( ~ c3_c4 + usda_code)
+# 
+# 
+# ## height x weeks: species (v1, straight line)
+# ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+#   geom_boxplot () +
+#   facet_wrap(~species_code)
+# 
+# ## height x weeks: species 
+# ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+#   geom_boxplot(alpha = 0.8) +
+#   geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+#   geom_smooth(aes(group = 1), method = "lm", color = "black", se = FALSE) +
+#   facet_wrap(~ species_code) +
+#   scale_fill_grey(start = 0.9, end = 0.3)
+# 
+# 
+# ## height x weeks: species (v2, curved lines)
+# ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+#   geom_boxplot(alpha = 0.8) +
+#   geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+#   geom_smooth(aes(group = 1), method = "loess", color = "black", se = TRUE) +
+#   facet_wrap(~ species_code) +
+#   scale_fill_grey(start = 0.9, end = 0.3)
+# 
+# 
+# ## height x weeks: species (v3, different shades)
+# ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+#   geom_boxplot(alpha = 0.8) +
+#   geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+#   geom_smooth(aes(color = trt_light, group = trt_light),
+#               method = "lm", se = FALSE) +
+#   facet_wrap(~ species_code) +
+#   scale_fill_grey(start = 0.9, end = 0.3)
+# 
+# 
+# ## height x weeks: species (v4, different shades, cute curves)
+# ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
+#   geom_boxplot(alpha = 0.8) +
+#   geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
+#   geom_smooth(aes(color = trt_light, group = trt_light),
+#               method = "loess", se = FALSE) +
+#   facet_wrap(~ species_code) +
+#   scale_fill_grey(start = 0.9, end = 0.3)
 
-## c3 vs. c4 plant height
-ggplot(data_pheno_heights_weeks_filtered, 
-       aes(x = as.factor(week), 
-           y = X8_height_cm, 
-           fill = as.factor(phosphorous_p1.p4))) +
-  geom_boxplot() +
-  facet_wrap( ~ c3_c4)
 
 
-
-## species plant height
-ggplot(data_pheno_heights_weeks_filtered, 
-       aes(x = as.factor(week), 
-           y = X7_leaf_count, 
-           fill = as.factor(phosphorous_p1.p4))) +
-  geom_boxplot() +
-  facet_wrap( ~ c3_c4 + usda_code)
-
-
-## height x weeks: species (v1, straight line)
-ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
-  geom_boxplot () +
-  facet_wrap(~species_code)
-
-## height x weeks: species 
-ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
-  geom_boxplot(alpha = 0.8) +
-  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
-  geom_smooth(aes(group = 1), method = "lm", color = "black", se = FALSE) +
-  facet_wrap(~ species_code) +
-  scale_fill_grey(start = 0.9, end = 0.3)
-
-
-## height x weeks: species (v2, curved lines)
-ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
-  geom_boxplot(alpha = 0.8) +
-  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
-  geom_smooth(aes(group = 1), method = "loess", color = "black", se = TRUE) +
-  facet_wrap(~ species_code) +
-  scale_fill_grey(start = 0.9, end = 0.3)
-
-
-## height x weeks: species (v3, different shades)
-ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
-  geom_boxplot(alpha = 0.8) +
-  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
-  geom_smooth(aes(color = trt_light, group = trt_light),
-              method = "lm", se = FALSE) +
-  facet_wrap(~ species_code) +
-  scale_fill_grey(start = 0.9, end = 0.3)
-
-
-## height x weeks: species (v4, different shades, cute curves)
+## height x weeks: species (v4, different shades, cute curves) ----------------# fig.1
 ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
   geom_boxplot(alpha = 0.8) +
   geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
   geom_smooth(aes(color = trt_light, group = trt_light),
               method = "loess", se = FALSE) +
   facet_wrap(~ species_code) +
-  scale_fill_grey(start = 0.9, end = 0.3)
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Height per week by species (treatments P and Light)") +
+  theme_bw(base_size = 25)
 
 
 
-## height x weeks: species (v4, different shades, cute curves) ----------------# fig.3
-ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
-  geom_boxplot(alpha = 0.8) +
-  geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
-  geom_smooth(aes(color = trt_light, group = trt_light),
-              method = "loess", se = FALSE) +
-  facet_wrap(~ species_code) +
-  scale_fill_grey(start = 0.9, end = 0.3)
-
-
-
-
-## height x weeks: species (v4, different shades, cute curves) ----------------# fig.4
+## height x weeks: species (v4, different shades, cute curves) ----------------# fig.1a
 ggplot(filtered_data_p1_p4, aes(x = week_number, y = height, fill = trt_p)) +
   geom_boxplot(alpha = 0.8) +
   geom_jitter(aes(color = trt_light), width = 0.2, alpha = 0.5) +
   geom_smooth(aes(color = trt_light, group = trt_light),
               method = "loess", se = FALSE) +
   facet_wrap(~ species_code) +
-  scale_fill_grey(start = 0.9, end = 0.3)
-
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Height per week by species ALL P-Treatments") +
+  theme_bw(base_size = 18)
 
 
 
@@ -248,22 +249,26 @@ ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
   scale_fill_grey(start = 0.9, end = 0.3)
 
 
-## height x weeks: shade treatments (v4) -------------------------------------## fig.1) shade x trt_p (w/ trends)
+## height x weeks: shade treatments (v4) -------------------------------------## fig. 2
 ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
   geom_boxplot(alpha = 0.8) +
   geom_smooth(aes(color = trt_p, group = trt_p),
               method = "loess", se = FALSE) +
   facet_grid(~ trt_light) +
-  scale_fill_grey(start = 0.9, end = 0.3)
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Height per week sun vs shade (p treatment)") +
+  theme_bw(base_size = 25)
 
 
-## height x weeks: shade treatments (v4) -------------------------------------## fig.2) shade x trt p x species (w/ trends)
+## height x weeks: shade treatments (v4) -------------------------------------## fig.3
 ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
   geom_boxplot(alpha = 0.8) +
   geom_smooth(aes(color = trt_p, group = trt_p),
               method = "loess", se = FALSE) +
   facet_wrap(~ species_code) +
-  scale_fill_grey(start = 0.9, end = 0.3)
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Height per week by species (p treatments)") +
+  theme_bw(base_size = 25)
 
 
 
@@ -272,4 +277,147 @@ ggplot(filtered_data, aes(x = week_number, y = height, fill = trt_p)) +
 
 
 
-# li600 ------------------------------------------------------------------------
+# Biomass ----------------------------------------------------------------------
+## root shoot ratio 
+biomass_total$root_shoot_ratio <- biomass_total$biomass_roots_g/ 
+  biomass_total$biomass_shoots_g
+
+
+## figures ---------------------------------------------------------------------
+
+biomass_total
+
+##  biomass roots ------------------------------------------------------------## fig. 4
+ggplot(biomass_total,
+       aes(phosphorous_p1.p4, biomass_roots_g, 
+           fill = phosphorous_p1.p4)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = shade_00s_70s), width = 0.2, alpha = 0.7) +
+  geom_smooth(aes(color = shade_00s_70s, group = shade_00s_70s),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Biomass roots") +
+  theme_bw(base_size = 25)
+
+
+##  biomass shoots ------------------------------------------------------------## fig. 5
+ggplot(biomass_total,
+       aes(phosphorous_p1.p4, biomass_shoots_g, 
+           fill = phosphorous_p1.p4)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = shade_00s_70s), width = 0.2, alpha = 0.7) +
+  geom_smooth(aes(color = shade_00s_70s, group = shade_00s_70s),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Biomass shoots") +
+  theme_bw(base_size = 25)
+
+
+##  biomass root:shoot --------------------------------------------------------## fig. 6
+ggplot(biomass_total,
+       aes(phosphorous_p1.p4, root_shoot_ratio, 
+           fill = phosphorous_p1.p4)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = shade_00s_70s), width = 0.2, alpha = 0.7) +
+  geom_smooth(aes(color = shade_00s_70s, group = shade_00s_70s),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Biomass root:shoot ratio") +
+  theme_bw(base_size = 25)
+
+
+# 
+# ## roots
+# ggplot(data_biomass, aes(x = phosphorous_p1.p4, y = biomass_roots_g, 
+#                          fill = shade_00s_70s)) +
+#   geom_boxplot(outlier.shape = NA) +
+#   facet_wrap(~ c3_c4 ~ shade_00s_70s) +
+#   theme_minimal()
+# 
+# 
+# ## shoots
+# ggplot(data_biomass, aes(x = phosphorous_p1.p4, y = biomass_shoots_g, 
+#                          fill = shade_00s_70s)) +
+#   geom_boxplot(outlier.shape = NA) +
+#   facet_wrap(~ c3_c4 ~ shade_00s_70s) + 
+#   theme_minimal()
+# 
+# 
+# ## biomass volume ratio
+# data_biomass$biomass_pot_volume_ratio
+# ggplot(data_biomass, aes(x = phosphorous_p1.p4, y = biomass_pot_volume_ratio, 
+#                          fill = shade_00s_70s)) +
+#   geom_boxplot() +
+#   facet_wrap(~ c3_c4 ~ shade_00s_70s) + 
+#   theme_minimal()
+
+
+# Pot binding ------------------------------------------------------------------
+## cleaning --------------------------------------------------------------------
+shoots$type <- NULL
+roots$type <- NULL
+biomass_total <- full_join(shoots, roots, by = "individual")
+
+
+## calculations ---------------------------------------------------------------#
+## total biomass count
+biomass_total$biomass_total <- biomass_total$biomass_shoots_g + 
+  biomass_total$biomass_roots_g
+
+## total plant biomass to pot volume ratio (goal 1 g L-1; Poorter et al 2012)
+biomass_total$biomass_pot_volume_ratio <- biomass_total$biomass_total / 1.589
+
+## add in metadata 
+biomass_total <- left_join(biomass_total, meta_data, by = "individual")
+
+
+## figures ---------------------------------------------------------------------
+
+## pot binding ratio plotted --------------------------------------------------# fig.0
+ggplot(biomass_total,
+       aes(phosphorous_p1.p4, biomass_pot_volume_ratio, 
+           fill = phosphorous_p1.p4)) +
+  geom_boxplot() +
+  geom_jitter(aes(color = shade_00s_70s), width = 0.2, alpha = 0.7) +
+  geom_smooth(aes(color = shade_00s_70s, group = shade_00s_70s),
+              method = "loess", se = FALSE) +
+  facet_wrap(~ species_code) +
+  scale_fill_grey(start = 0.9, end = 0.3) +
+  labs(title = "Pot binding, biomass volume to pot ratio by p treatment") +
+  theme_bw(base_size = 25)
+
+
+# species, anybody weird
+ggplot(biomass_total, aes(phosphorous_p1.p4, biomass_pot_volume_ratio)) + 
+  geom_boxplot(outlier.shape = NA) + 
+  geom_jitter(aes(color = species_code, shape = species_code), 
+              size = 3)
+
+
+
+# write.csv(biomass_total, "r_data/biomass_total.csv")
+
+########### 
+# look at the young growing height; were the young plants still pot bound; 
+# did we see the same pattern. 
+
+# look at heights, preveent. Discussion. 
+
+# flowering, they hit their max size. 
+
+# height, during the lifespan. Height platues. first p (non 0 P. 
+# consistnetly throughtout their lifespan. If oyu see a linear trend and then a
+# platue when they are older. 
+
+# max P reached. 
+
+# height data overtime.
+# easy to extract data/ do we see the. 
+
+# low light= needs less P. evidence. traits being more similar in 0 to 2nd p. 
+
+# soil ll/ HL
+## pot binding should have negetated.
